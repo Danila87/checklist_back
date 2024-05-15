9734f99cfa05dca16a2e0e9eae789638bbaefae0
DanilaS <d.sergeev@mfc38.ru>
2024-05-15 17:39:20 +0800

    test

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-requests.post('http://pipi.dev.corp/auth/login',
-              json={
-                  "login": "string",
-                  "password": "string"
-              })
-
-print(requests)
